5306b71fc066cd055c0a742f45d94ba03952494e
David <davidcking.inbox@gmail.com>
2022-02-13 17:01:34 -0700

    Added documentation.

--- R/c_elegans.R
+++ R/c_elegans.R
@@ -1,3 +1,12 @@
+#' Return C. elegans protein coding genes as a Genomic Ranges object.
+#'
+#' Run a preformatted query to get C. elegans protein coding gene locations, with options to subset the
+#' query by gene name or wormbase gene IDs.
+#' @param mart A 'mart' object as returned by 'getParamart'.
+#' @param gene_list Retrieve only genomic locations for gene names specified here, rather than the whole genome.
+#' @param WBID Retrieve only genomic locations for Wormbase IDs specified here, rather than the whole genome.
+#' @return A Genomic Ranges object.
+#' @seealso \code{\link{getCElegansPromoters}}, \code{\link{getParamart}}, \code{\link[GenomicRanges]{GenomicRanges}}
 #' @export
 getCElegansGeneLocs = function(mart, gene_list=NULL, WBID=NULL) {
   QUERY='<?xml version="1.0" encoding="UTF-8"?>
@@ -34,6 +43,9 @@ getCElegansGeneLocs = function(mart, gene_list=NULL, WBID=NULL) {
   GenomeInfoDb::seqinfo(gr) <- GenomeInfoDb::Seqinfo(genome='ce11')
   gr
 }
+#' Return promoter ranges based on arguments, applied to the result of getCElegansGeneLocs.
+#' 
+#' 
 #' @export
 getCElegansPromoters = function(mart, upstream=2000,downstream=500,gene_list=NULL, WBID=NULL) {
   genes_df = getCElegansGeneLocs(mart, gene_list=NULL, WBID=NULL)
